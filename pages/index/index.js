999f7cccc7aee1178b1fb5f234a4e615c621c775
shengye.lv <739352820@qq.com>
2019-06-11 21:03:59 +0800

    首页修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -19,13 +19,22 @@ Page({
     autoplay: true,
     interval: 3000,
     duration: 500,
-
-  
   },
-  
+  //下拉触发事件
+  onPullDownRefresh: function () {
+    this.loadData(this.data.pageNo )
+  },
+  //上拉拉到底触发的事件
+  onReachBottom: function () {
+    if(this.data.pageNo-1==0){
+         return
+    }
+    this.loadData(this.data.pageNo-1)
+  },
+
   //事件处理函数
   gotoCustomInfo: function () {
-    app.navTo('customInfo')
+   this.loadData(this.data.pageNo+1)
   },
 
   /**
@@ -33,14 +42,6 @@ Page({
    */
   viewPositionDetail: (e) => {
     app.navTo('positionDetail', {positionId: e.currentTarget.dataset.pid})
-  },
-
-  /**
-   * 加载更多
-   *
-   */
-  loadMore() {
-    this.loadData(this.data.pageNo + 1)
   },
   /**
    * 通用请求数据函数
@@ -50,161 +51,43 @@ Page({
   loadData: function (e) {
     var that = this;
     wx.request({
-      url: 'http://localhost:8080/test', // 仅为示例，并非真实的接口地址
-      data: {
-        pageNo: e
-      },
+      url: 'http://localhost:8080/position/getPositionOfPage/'+e, // 仅为示例，并非真实的接口地址
       header: {
         'content-type': 'application/json' // 默认值
       },
       success(res) {
-        console.log(res.data);
         that.setData({
           positionList: [{
-            companyFullName: "行知成教育科技有限公司",
-            city: "甘肃省内、省外",
+            companyLogo:"sss",
+            companyName: "行知成教育科技有限公司",
+            workAddress: "甘肃省内、省外",
             salary: "80-100",
-            createTime: 2019
-
-          },
-          {
-            companyFullName: "黑龙江科技大学",
-            city: "哈尔滨",
-            salary: "15元/日",
-            createTime: 2018
-          }]
+            beginTime: 2019
+          }],
+          pageNo:e
+          
         })
-
-
       },
-      fail(e) {
-        console.log(e.errMsg)
+      fail(err) {
+        wx.showToast({
+          title: '操作失败',
+          icon: 'none',
+          duration: 2000
+        }),    
         that.setData({
           positionList: [{
-            companyFullName: "营地导师",
-            city: "甘肃省内、省外",
-            salary: "80-100/日",
-            createTime: 2019,
-            companyLogo:"../../static/images/5.jpg"
-          },
-            {
-              companyFullName: "营地导师",
-              city: "哈尔滨",
-              salary: "150/日",
-              createTime: 2019,
-              companyLogo: "../../static/images/6.jpg"
-            },
-            {
-              companyFullName: "营地导师",
-              city: "沈阳",
-              salary: "100/日",
-              createTime: 2019,
-              companyLogo: "../../static/images/4.jpg"
-            }, {
-              companyFullName: "营地导师",
-              city: "甘肃省内、省外",
-              salary: "80-100/日",
-              createTime: 2019,
-              companyLogo: "../../static/images/5.jpg"
-            }, {
-              companyFullName: "营地导师",
-              city: "甘肃省内、省外",
-              salary: "80-100/日",
-              createTime: 2019,
-              companyLogo: "../../static/images/5.jpg"
-            }, {
-              companyFullName: "营地导师",
-              city: "甘肃省内、省外",
-              salary: "80-100/日",
-              createTime: 2019,
-              companyLogo: "../../static/images/5.jpg"
-            }, {
-              companyFullName: "营地导师",
-              city: "甘肃省内、省外",
-              salary: "80-100/日",
-              createTime: 2019,
-              companyLogo: "../../static/images/5.jpg"
-            },
-            {
-              companyFullName: "营地导师",
-              city: "哈尔滨",
-              salary: "150/日",
-              createTime: 2019,
-              companyLogo: "../../static/images/6.jpg"
-            },
-            {
-              companyFullName: "营地导师",
-              city: "沈阳",
-              salary: "100/日",
-              createTime: 2019,
-              companyLogo: "../../static/images/4.jpg"
-            }, {
-              companyFullName: "营地导师",
-              city: "甘肃省内、省外",
-              salary: "80-100/日",
-              createTime: 2019,
-              companyLogo: "../../static/images/5.jpg"
-            },
-            {
-              companyFullName: "营地导师",
-              city: "哈尔滨",
-              salary: "150/日",
-              createTime: 2019,
-              companyLogo: "../../static/images/6.jpg"
-            },
-            {
-              companyFullName: "营地导师",
-              city: "沈阳",
-              salary: "100/日",
-              createTime: 2019,
-              companyLogo: "../../static/images/4.jpg"
-            }, {
-              companyFullName: "营地导师",
-              city: "甘肃省内、省外",
-              salary: "80-100/日",
-              createTime: 2019,
-              companyLogo: "../../static/images/5.jpg"
-            },
-            {
-              companyFullName: "营地导师",
-              city: "哈尔滨",
-              salary: "150/日",
-              createTime: 2019,
-              companyLogo: "../../static/images/6.jpg"
-            },
-            {
-              companyFullName: "营地导师",
-              city: "沈阳",
-              salary: "100/日",
-              createTime: 2019,
-              companyLogo: "../../static/images/4.jpg"
-            }, {
-              companyFullName: "营地导师",
-              city: "甘肃省内、省外",
-              salary: "80-100/日",
-              createTime: 2019,
-              companyLogo: "../../static/images/5.jpg"
-            },
-            {
-              companyFullName: "营地导师",
-              city: "哈尔滨",
-              salary: "150/日",
-              createTime: 2019,
-              companyLogo: "../../static/images/6.jpg"
-            },
-            {
-              companyFullName: "营地导师",
-              city: "沈阳",
-              salary: "100/日",
-              createTime: 2019,
-              companyLogo: "../../static/images/4.jpg"
-            }]
-        })
+            companyLogo: "sss",
+            companyName: "行知成教育科技有限公司",
+            workAddress: "甘肃省内、省外",
+            salary: "80-100",
+            beginTime: 2019
+          }
+          ]
+      })
       }
     })
   },
-  onLoad: function () {
-    this.loadData()
-    
+  onShow: function () {
+    this.loadData(this.data.pageNo)
   }
 })
